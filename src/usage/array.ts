25df7d53cacb0661828cb0d29da32ab379ab6c18
AgustinSRG <41982624+AgustinSRG@users.noreply.github.com>
2021-06-18 16:35:26 +0200

    Schema for array

---
+++ src/usage/array.ts
@@ -0,0 +1,53 @@
+// Javascript object sanitizer
+
+"use strict";
+
+import { ArrayRawSchema } from "../schema-raw";
+import { AbstractObjectSchema, ObjectSchemaI } from "./schema";
+
+/**
+ * Schema for Array
+ */
+export class ArrayObjectSchema extends AbstractObjectSchema {
+    /**
+     * Creates array schema
+     * @param itemsSchema Schema for the array items
+     * @returns a new instance of ArrayObjectSchema
+     */
+    public static create(itemsSchema: ObjectSchemaI): ArrayObjectSchema {
+        return new ArrayObjectSchema(itemsSchema);
+    }
+
+    private schema: ArrayRawSchema;
+
+    /**
+     * Constructor
+     */
+    constructor(itemsSchema: ObjectSchemaI) {
+        super({
+            $type: "array",
+            $items: itemsSchema.getRawSchema(),
+        });
+        this.schema = <ArrayRawSchema>this.getRawSchema();
+    }
+
+    /**
+     * Sets default value, in case the object is null, undefined or cannot be parsed
+     * @param defaultValue The default value
+     * @returns self
+     */
+    public setDefaultValue(defaultValue: any[]): ArrayObjectSchema {
+        this.schema.$default = defaultValue;
+        return this;
+    }
+
+    /**
+     * Sets max length for the array. If the array length is greater than this value, it will be truncated.
+     * @param maxLength The max length for the array.
+     * @returns self
+     */
+    public setMaxLength(maxLength: number): ArrayObjectSchema {
+        this.schema.$maxLength = maxLength;
+        return this;
+    }
+}
